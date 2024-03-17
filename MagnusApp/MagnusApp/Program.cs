fd218384d542d7da6f07f52eee156e3f93944ae2
Magnus Mutai <MagnusMq07@gmail.com>
2024-03-17 23:15:51 +0300

    Done a great job after a great series of turbulence in my career

--- MagnusApp/MagnusApp/Program.cs
+++ MagnusApp/MagnusApp/Program.cs
@@ -1,12 +1,17 @@
 using MagnusApp.Client.Pages;
 using MagnusApp.Components;
+using Syncfusion.Blazor;
 
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXxfd3VQRmBcWExwXEU=");
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
