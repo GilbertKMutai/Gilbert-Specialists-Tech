b666b4202e94177edc740c946cc4c06b72102baa
Magnus Mutai <MagnusMq07@gmail.com>
2024-04-15 22:14:14 +0300

    Correction of things I did not like going on well

--- MagnusApp.Shared/ClientInfo.cs
+++ MagnusApp.Shared/ClientInfo.cs
@@ -12,7 +12,8 @@ namespace MagnusApp.Shared
         [Required (ErrorMessage = "The field Name is required")]
         //[Display(Name = "Full Name")]
         public string Name { get; set; }
-        [EmailAddress(ErrorMessage = "Please enter an Email Address")]
+        [Required (ErrorMessage ="Please enter an Email Address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email Address")]
         //[Display(Name = "Email Address")]
         public string Email { get; set; }
         [Required(ErrorMessage ="Please enter a message")]
