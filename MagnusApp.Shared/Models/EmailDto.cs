84d483256711385c8b3270d15ae31b842ea0b34b
Magnus Mutai <MagnusMq07@gmail.com>
2024-04-21 14:18:07 +0300

    Great progress

--- MagnusApp.Shared/Models/EmailDto.cs
+++ MagnusApp.Shared/Models/EmailDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace MagnusApp.Shared.Models
 {
     public class EmailDto
     {
-        public string From { get; set; } = string.Empty;
+        [Required(ErrorMessage = "The field Name is required")]
         public string Subject { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter an Email Address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email Address")]
+        public string From { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a message")]
         public string Body { get; set; } = string.Empty;
 
     }
--- MagnusApp.Shared/Services/EmailService/EmailService.cs
+++ MagnusApp.Shared/Services/EmailService/EmailService.cs
@@ -19,11 +19,14 @@ namespace MagnusApp.Shared.Services.EmailService
 
         public void SendEmail(EmailDto request)
         {
+            string text = $"{request.Body}" + $"#{request.From}";
+            text = text.Replace("#", Environment.NewLine);
+
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse(request.From));
             email.To.Add(MailboxAddress.Parse("mutaigilly02@gmail.com"));
             email.Subject = request.Subject;
-            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+            email.Body = new TextPart(TextFormat.Html) { Text = text };
 
             using var smtp = new SmtpClient();
             smtp.Connect(mailSettings.EmailHost, 587, MailKit.Security.SecureSocketOptions.StartTls);
