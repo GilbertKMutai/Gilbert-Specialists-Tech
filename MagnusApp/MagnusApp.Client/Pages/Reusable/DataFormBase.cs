a16a4f26ef343e38c2a002d5b7a07abde318b02f
Magnus Mutai <MagnusMq07@gmail.com>
2024-04-27 22:20:25 +0300

    Great progress

--- MagnusApp/MagnusApp.Client/Pages/Reusable/DataFormBase.cs
+++ MagnusApp/MagnusApp.Client/Pages/Reusable/DataFormBase.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Syncfusion.Blazor.DataForm;
 
 namespace MagnusApp.Client.Pages.Reusable
 {
@@ -6,5 +8,21 @@ namespace MagnusApp.Client.Pages.Reusable
     {
         [CascadingParameter]
         public EmailDto ClientModel { get; set; }
+
+        public SfDataForm dataForm { get; set; }
+
+        [Inject]
+        public IEmailService EmailService { get; set; }
+
+        [Parameter]
+        public EventCallback<EmailDto> OnFormSubmited { get; set; }
+
+        protected async void HandleValidSubmit()
+        {
+            await EmailService.SendEmail(ClientModel);
+            await OnFormSubmited.InvokeAsync(ClientModel);
+            ClientModel = new EmailDto();
+            dataForm.Refresh();
+        }
     }
 }
