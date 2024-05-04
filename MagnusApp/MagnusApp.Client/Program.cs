fadc6c38f9d8fbf7c1667e16c92058845a115a6a
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-04 22:23:11 +0300

    Adding individual accounts authentication manually

--- MagnusApp/MagnusApp.Client/Program.cs
+++ MagnusApp/MagnusApp.Client/Program.cs
@@ -2,6 +2,8 @@ global using MagnusApp.Shared.Services.EmailService;
 global using MagnusApp.Shared.Models;
 global using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using MagnusApp.Client;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +12,10 @@ builder.Services.AddHttpClient<IEmailService, EmailService>(client =>
 {
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
 });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
 //Gets values from appsettings.json
 //var clientSecret = builder.Configuration.GetValue<string>("MailSettings:EmailHost");
