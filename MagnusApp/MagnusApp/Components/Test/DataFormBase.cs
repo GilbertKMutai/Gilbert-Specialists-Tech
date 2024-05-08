d83ea9572ac1be18faa09a27e1e2514211a04f51
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-08 08:31:35 +0300

    The problem with the Login page is interactivity

---
+++ MagnusApp/MagnusApp/Components/Test/DataFormBase.cs
@@ -0,0 +1,24 @@
+﻿using MagnusApp.Shared.Models;
+using MagnusApp.Shared.Services.EmailService;
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.DataForm;
+namespace MagnusApp.Components.Test;
+
+public class DataFormBase : ComponentBase
+{
+    public EmailDto ClientModel = new EmailDto();
+
+    public SfDataForm dataForm { get; set; }
+
+    [Inject]
+    public IEmailService EmailService { get; set; }
+
+    protected bool RememberMe { get; set; } = false;
+
+    protected async void HandleValidSubmit()
+    {
+        await EmailService.SendEmail(ClientModel);
+        ClientModel = new EmailDto();
+        dataForm.Refresh();
+    }
+}
