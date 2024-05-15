c04da4264916920bdd89926992f1dbcc36f1f24c
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-15 07:17:59 +0300

    Added migrations to make lastname and firstname nullable updated database

--- MagnusApp/MagnusApp/Data/ApplicationUser.cs
+++ MagnusApp/MagnusApp/Data/ApplicationUser.cs
@@ -5,8 +5,8 @@ namespace MagnusApp.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
     }
 
 }
