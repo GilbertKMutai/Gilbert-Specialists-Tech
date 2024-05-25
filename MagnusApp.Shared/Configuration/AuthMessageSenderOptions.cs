888d60395dba77fccd3d103d9c772edf809bf352
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-25 23:59:23 +0300

    Adding Email Confirmation with MailChimp for Microsoft Identity

---
+++ MagnusApp.Shared/Configuration/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagnusApp.Shared.Configuration
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? EmailAuthKey { get; set; }
+    }
+}
