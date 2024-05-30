be8ae66bcdc685975e060307f203ad1a1841d777
Magnus Mutai <MagnusMq07@gmail.com>
2024-05-30 08:33:14 +0300

    Configuration error and Mandrill error I know why configuration error

--- MagnusApp.Shared/Configuration/AuthMessageSenderOptions.cs
+++ MagnusApp.Shared/Configuration/AuthMessageSenderOptions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace MagnusApp.Shared.Configuration
 {
     public class AuthMessageSenderOptions
     {
-        public required string EmailAuthKey { get; set; }
+
+        //[Inject]
+        //public IConfiguration configuration1 { get; set; }
+        //public string EmailAuthKey => configuration1["MailChimpAuthKey"]!;
+        public string EmailAuthKey { get; set; } = "974a1041e36f7a0d807fbc5d788c741c-us17";
     }
 }
