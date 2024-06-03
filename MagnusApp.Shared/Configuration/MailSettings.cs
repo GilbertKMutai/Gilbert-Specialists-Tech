ad5d04be82e78c84cf60918f07639384fd6d671c
Magnus Mutai <MagnusMq07@gmail.com>
2024-06-03 18:23:02 +0300

    Doing configuration at production level with appsetiings

--- MagnusApp.Shared/Configuration/MailSettings.cs
+++ MagnusApp.Shared/Configuration/MailSettings.cs
@@ -10,11 +10,14 @@ public class MailSettings : IMailSettings
         this.configuration = configuration;
     }
 
-    public string EmailHost => configuration["Google_EmailHost"]!;
-    public string EmailUserName => configuration["Google_EmailUserName"]!;
-    public string EmailPassword => configuration["Google_EmailPassword"]!;
+    //Get secrets from secrets.json/appsettings.json
+    public string EmailHost => configuration["MailSettings:EmailHost"]!;
+    public string EmailUserName => configuration["MailSettings:EmailUserName"]!;
+    public string EmailPassword => configuration["MailSettings:EmailPassword"]!;
 
-    //public string EmailHost => configuration["MailSettings:EmailHost"]!;  
-    //public string EmailUserName => configuration["MailSettings:EmailUserName"]!;
-    //public string EmailPassword => configuration["MailSettings:EmailPassword"]!;
+    //TODO
+    //Get secrets from aws secrets manager
+    //public string EmailHost => configuration["Google_EmailHost"]!;
+    //public string EmailUserName => configuration["Google_EmailUserName"]!;
+    //public string EmailPassword => configuration["Google_EmailPassword"]!;
 }
