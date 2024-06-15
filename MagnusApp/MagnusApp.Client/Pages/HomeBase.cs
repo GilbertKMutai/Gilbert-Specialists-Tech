6d35b95bee8fd75add0ce06587e3a305828b9952
Magnus Mutai <MagnusMq07@gmail.com>
2024-06-16 00:04:26 +0300

    reinstated spinner in the home page for publishing of other unrelated upgrades

--- MagnusApp/MagnusApp.Client/Pages/HomeBase.cs
+++ MagnusApp/MagnusApp.Client/Pages/HomeBase.cs
@@ -2,14 +2,14 @@
 
 public class HomeBase:ComponentBase
 {
-    protected string? CssClass { get; set; } = "spin-container";
+    protected string? Cssclass { get; set; } = "spinner-container";
     protected bool IsVisible { get; set; } = true;
 
     protected override async Task OnInitializedAsync()
     {
         await Task.Delay(3500);
         IsVisible = false;
-        CssClass = null;
+        Cssclass = null;
     }
 
 }
