ddbe2502341fd8ea5466032ef4be14268505feaf
Magnus Mutai <MagnusMq07@gmail.com>
2024-07-25 20:59:00 +0300

    removed js interop function from homepage

--- MagnusApp/MagnusApp.Client/Pages/HomeBase.cs
+++ MagnusApp/MagnusApp.Client/Pages/HomeBase.cs
@@ -4,7 +4,6 @@ namespace MagnusApp.Client.Pages;
 
 public class HomeBase:ComponentBase
 {
-    public bool firstRender = true;
     //protected string? Cssclass { get; set; } = "spinner-container";
     //protected bool IsVisible { get; set; } = true;
 
@@ -14,21 +13,5 @@ public class HomeBase:ComponentBase
     //    IsVisible = false;
     //    Cssclass = null;
     //}
-
-    [Inject]
-    public IJSRuntime JSRuntime { get; set; }
-
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-
-            //await JSRuntime.InvokeAsync<string>("addEventListener");
-            //await JSRuntime.InvokeAsync<string>("onscroll");
-            //StateHasChanged();
-        }
-    }
-
-
 
 }
