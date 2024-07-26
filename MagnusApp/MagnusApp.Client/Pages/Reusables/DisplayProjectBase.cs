7ee290dce1ed337ae6289c389c44b57ace3c1ba6
Magnus Mutai <MagnusMq07@gmail.com>
2024-07-26 23:20:13 +0300

    removed test animation methods from  displayprojects page and redundant js function in app.razor Removed the js interop function in Display project Added interactive wasm to home, project and display project components

--- MagnusApp/MagnusApp.Client/Pages/Reusables/DisplayProjectBase.cs
+++ MagnusApp/MagnusApp.Client/Pages/Reusables/DisplayProjectBase.cs
@@ -10,37 +10,10 @@ public class DisplayProjectBase : ComponentBase
     [Inject]
     NavigationManager NavigationManager { get; set; }
 
-    [Inject]
-    public IJSRuntime JSRuntime { get; set; }
 
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        if (firstRender)
-        {
-            await JSRuntime.InvokeAsync<string>("addEventListener");
-        }
-    }
-
-    protected async void Clicktoanimate()
-    {
-        await JSRuntime.InvokeAsync<string>("CallMethod");
-    }
 
     protected void OpenWebsite(string uri)
     {
         NavigationManager.NavigateTo(uri, false);
-    }
-
-    [JSInvokable]
-    public static void AddAnimation()
-    {
-        DisplayProjectBase displayProjectBase = new DisplayProjectBase();
-        displayProjectBase.Css = "show-animate";
-    }
-
-
-    public async void HelloWorld()
-    {
-        await JSRuntime.InvokeVoidAsync("HelloWorld");
     }
 }
