b63f702a24024275b2ce589c5bff29705ac5a2f0
Magnus Mutai <MagnusMq07@gmail.com>
2024-09-15 12:05:09 +0300

    Adding JavaScript code to disable Carousel Autoplay

---
+++ MagnusApp/MagnusApp.Client/Pages/Reusables/DisplayTestimonialBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+using Syncfusion.Blazor.Navigations;
+
+namespace MagnusApp.Client.Pages.Reusables
+{
+    public class DisplayTestimonialBase : ComponentBase
+    {
+
+        protected SfCarousel carouselRef;
+        protected static bool carouselAutoPlay = true;
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+
+
+        [JSInvokable]
+        private static void DisableAutoPlay()
+        {
+            // Disable autoplay for screen sizes greater than or equal to 1000px
+            carouselAutoPlay = false;
+
+            //InvokeAsync(StateHasChanged);
+        }
+
+    }
+}
